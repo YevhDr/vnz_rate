fb187c892fe67dc1cd8e22ab7e26c722ce67acdb
YevhDr <evgesha.drozdova@gmail.com>
2017-11-28 16:01:57 +0200

    +

--- js/main-table.js
+++ js/main-table.js
@@ -149,34 +149,34 @@ d3.csv(file, function (error, data) {
 
 
 
-    // paginationList(20);
-    //
-    //
-    // function paginationList(listPaginationCount){
-    //     $('table.paginated').each(function() {
-    //         var currentPage = 0;
-    //         var numPerPage = listPaginationCount;
-    //         var $pager=$('.pager').remove();
-    //         var $table = $(this);
-    //         $table.bind('repaginate', function() {
-    //             $table.find('tbody tr').hide().slice(currentPage * numPerPage, (currentPage + 1) * numPerPage).show();
-    //         });
-    //         $table.trigger('repaginate');
-    //         var numRows = $table.find('tbody tr').length;
-    //         var numPages = Math.ceil(numRows / numPerPage);
-    //         var $pager = $('<div class="pager"></div>');
-    //         for (var page = 0; page < numPages; page++) {
-    //             $('<span class="page-number"></span>').text(page + 1).bind('click', {
-    //                 newPage: page
-    //             }, function(event) {
-    //                 currentPage = event.data['newPage'];
-    //                 $table.trigger('repaginate');
-    //                 $(this).addClass('active').siblings().removeClass('active');
-    //             }).appendTo($pager).addClass('clickable');
-    //         }
-    //         $pager.insertBefore($table).find('span.page-number:first').addClass('active');
-    //     });
-    // }
+    paginationList(20);
+
+
+    function paginationList(listPaginationCount){
+        $('table.paginated').each(function() {
+            var currentPage = 0;
+            var numPerPage = listPaginationCount;
+            var $pager=$('.pager').remove();
+            var $table = $(this);
+            $table.bind('repaginate', function() {
+                $table.find('tbody tr').hide().slice(currentPage * numPerPage, (currentPage + 1) * numPerPage).show();
+            });
+            $table.trigger('repaginate');
+            var numRows = $table.find('tbody tr').length;
+            var numPages = Math.ceil(numRows / numPerPage);
+            var $pager = $('<div class="pager"></div>');
+            for (var page = 0; page < numPages; page++) {
+                $('<span class="page-number"></span>').text(page + 1).bind('click', {
+                    newPage: page
+                }, function(event) {
+                    currentPage = event.data['newPage'];
+                    $table.trigger('repaginate');
+                    $(this).addClass('active').siblings().removeClass('active');
+                }).appendTo($pager).addClass('clickable');
+            }
+            $pager.insertBefore($table).find('span.page-number:first').addClass('active');
+        });
+    }
 
 
 //     // Create selection from Array
