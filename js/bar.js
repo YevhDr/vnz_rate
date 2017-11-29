72c94770b282585a0315d4802fb61e6e8e698718
YevhDr <evgesha.drozdova@gmail.com>
2017-11-29 15:13:25 +0200

    +

--- js/bar.js
+++ js/bar.js
@@ -1,93 +1,65 @@
-// var bar = function () {
-//
-//     function chart(selection) {
-//
-//         selection.each(function (data) {
-//
-//             var div = d3.select(this),
-//                 svg = div.selectAll('svg').data(d).enter().append('svg');
-//
-//         });
-//
-//         return chart;
-//     }
-//
-// };
-//
+var drawBar = function() {
+
+    var width = 100;
+    var height = 60;
+    var margin = {top: 0, bottom: 0, left:0, right:0};
+
+    function chart(selection) {
+
+        selection.each(function (data) {
+
+            var div = d3.select(this);
+            var svg = div.selectAll('svg')
+                .data([data])
+                .enter()
+                .append('svg');
+
+            svg.attr('width', width)
+                .attr('height', height - margin.top)
+                .style("overflow", "visible")
+                // .append("g")
+                // .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+            ;
 
 
+            var x = d3.scale.linear().range([0, width]).domain([0, 200]);
+
+            var y = d3.scale.linear().range([height, 0]);
+
+            var xAxis = d3.svg.axis().scale(x)
+                .orient("bottom").ticks(2);
+
+            svg.append("g")
+                .attr("class", "x axis")
+                .attr("transform", "translate(0," + height + ")")
+                // .call(xAxis)
+
+            ;
+
+            svg.selectAll("rect")
+                .data([data])
+                .enter()
+                .append("rect")
+                .attr("class", "bar")
+                .attr("x", x(data.from))
+                .attr("y", 40)
+                .attr("width", x(data.to - data.from))
+                .attr("height", 10)
+
+            ;
+
+            svg.append("text")
+                .data(data)
+                .attr("y", 30)
+                .attr("text-anchor", "start")
+                .style("fill", "black")
+                .html(data.mean)
+                .style("font-size", "1em")
+                .style("padding-top", "5px");
 
 
+        });
+    }
 
-// var bar = function () {
-//
-//     var width = 120;
-//     var height = 30;
-//     var margin = {top: 0, bottom: 0, left:0, right:0};
-//
-//     function chart(selection) {
-//
-//         selection.each(function (data) {
-//
-//             var div = d3.select(this),
-//                 svg = div.selectAll('svg').data([data]);
-//
-//             svg.enter()
-//                 .append('svg')
-//                 .attr('width', width)
-//                 .attr('height', height - margin.top)
-//                 .style("overflow", "visible")
-//                 .append("g")
-//                 .attr("transform", "translate(" + margin.left + "," + margin.top + ")");;
-//
-//             var y = d3.scale.ordinal()
-//                 .range([0, height]);
-//
-//             var x = d3.scale.linear()
-//                 .range([0, width]);
-//
-//
-//
-//             y.domain(0, 5);
-//             x.domain([d3.min(data,function(d){return d.from;}), d3.max(data,function(d){return d.to;})]);
-//
-//
-//
-//
-//             svg.append("g")
-//                 .attr("class", "x axis")
-//                 .attr("transform", "translate(0," + height + ")")
-//                 // .call(xAxis)
-//                 // .append("text")
-//                 // .attr("x", width-75)
-//                 // .attr("dx", ".71em")
-//                 // .attr("dy", "-.71em")
-//                 // .text("Temperatures (C)")
-//             ;
-//
-//             svg.append("g")
-//                 .attr("class", "y axis")
-//                 // .call(yAxis)
-//             ;
-//
-//             svg.selectAll(".bar")
-//                 .data(data)
-//                 .enter().append("rect")
-//                 .attr("class", "bar")
-//                 .attr("y", function(d) { return y(d.month); })
-//                 .attr("height", y.rangeBand())
-//                 .attr("x", function(d) { return x(d.from); })
-//                 .attr("width", function(d) { return x(d.to)-x(d.from) });
-//
-//
-//
-//
-//     }
-//     )
-// ;
-//
-//     return chart;
-//
-//  }
-//
-// };
+    return chart ;
+};
--- js/main-table.js
+++ js/main-table.js
@@ -58,74 +58,7 @@ d3.csv(file, function (error, data) {
         .datum(function (d) {
             return d;
         })
-        .call(function() {
-
-            var width = 120;
-            var height = 30;
-            var margin = {top: 0, bottom: 0, left:0, right:0};
-
-            function chart(selection) {
-
-                selection.each(function (data) {
-
-                    var div = d3.select(this);
-                    var svg = div.selectAll('svg')
-                        .data([data])
-                        .enter()
-                        .append('svg');
-
-                    svg.attr('width', width)
-                        .attr('height', height - margin.top)
-                        .style("overflow", "visible")
-                        .append("g")
-                        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-                    var y = d3.scale.ordinal()
-                        .range([0, height]);
-
-                    var x = d3.scale.linear()
-                        .range([0, width]);
-
-
-
-                    y.domain(0, 5);
-                    x.domain([d3.min(data,function(d){return d.from;}), d3.max(data,function(d){return d.to;})]);
-
-
-
-
-                    svg.append("g")
-                        .attr("class", "x axis")
-                        .attr("transform", "translate(0," + height + ")")
-                    // .call(xAxis)
-                    // .append("text")
-                    // .attr("x", width-75)
-                    // .attr("dx", ".71em")
-                    // .attr("dy", "-.71em")
-                    // .text("Temperatures (C)")
-                    ;
-
-                    svg.append("g")
-                        .attr("class", "y axis")
-                    // .call(yAxis)
-                    ;
-
-                    svg.selectAll(".bar")
-                        .data(data)
-                        .enter().append("rect")
-                        .attr("class", "bar")
-                        .attr("y", function(d) { return y(d.month); })
-                        .attr("height", y.rangeBand())
-                        .attr("x", function(d) { return x(d.from); })
-                        .attr("width", function(d) { return x(d.to)-x(d.from) });
-
-
-
-                });
-            }
-
-            return chart ;
-        });
+        .call(drawBar());
 
     rows.append('td').text(function (d) { return d.total_apps; });
     rows.append('td').text(function (d) { return d.total_per_place; });
